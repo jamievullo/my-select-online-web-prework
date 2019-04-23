5e66120312d5757daed05132dcda01609ac4384c
Jamie Vullo <jvullo@protonmail.com>
2019-04-23 17:17:41 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,11 +2,11 @@ def my_select(collection)
  new_array = []
  i = 0 
  while i < collection.length 
-   num = collection[i].num.even?
+   num = collection[i]
    new_array << yield(collection[i])
  
  
  i += 1 
  end
- return new_array
+ new_array
 end
